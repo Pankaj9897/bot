da2846e4b74061616f719762ec7d6c7c01626ddc
Pankaj9897 <joshipankaj9897@gmail.com>
2025-04-16 10:43:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,34 @@
 from flask import Flask, request, jsonify
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  
+
+API_KEY = os.getenv('API_KEY') 
+
 
 app = Flask(__name__)
-API_KEY = 'cur_live_wxR74ACQkIb6rYD1HicP0B3MDRw0B0Dn48lNo5Vf'
+
 
 @app.route('/', methods=['POST'])
 def convert_currency():
     data = request.get_json()
 
     if data is None:
-        print("❌ Koi JSON data nahi mila request se.")
+        print(" Koi JSON data nahi mila request se.")
         return jsonify({"fulfillmentText": "Bhai, JSON data hi nahi mila!"})
 
-    print("✅ JSON mila:", data)  # 👈 Add this
+    print("JSON mila:", data)  
 
     unit_currency = data['queryResult']['parameters']['unit-currency']
     source_currency = unit_currency[0]['currency']
     amount = unit_currency[0]['amount']
     target_currency = data['queryResult']['parameters']['currency-name']
     
-    print("🔁 Source:", source_currency)
-    print("💰 Amount:", amount)
-    print("🎯 Target:", target_currency)
+    print("Source:", source_currency)
+    print( Amount:", amount)
+    print("target:", target_currency)
 
     # Call CurrencyAPI
     url = f"https://api.currencyapi.com/v3/latest?apikey={API_KEY}&base_currency={source_currency}&currencies={target_currency}"
@@ -35,7 +42,7 @@ def convert_currency():
             "fulfillmentText": f"{amount} {source_currency} = {converted_amount:.2f} {target_currency}"
         })
     except Exception as e:
-        print("❌ Error in conversion:", e)
+        print("Error in conversion:", e)
         return jsonify({
             "fulfillmentText": "Kuch galat ho gaya bhai. Please check currency codes or API response."
         })
